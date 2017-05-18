6a6c821f0aa3df17d1c24da5587d704475ca7c13
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2017-05-18 23:45:00 +0200

    use --pid=host --net=host and --privileged=true as suggested in the nrsysmond docker image:
    See: https://github.com/christian-blades-cb/newrelic-docker/blob/master/newrelic.service

--- kontena-master-nrsysmond-deploy.sh
+++ kontena-master-nrsysmond-deploy.sh
@@ -13,18 +13,24 @@ kontena master ssh -i $EC2_KEYS_DIR/kontena.pem \
 # https://github.com/christian-blades-cb/newrelic-docker/blob/master/newrelic.service
 
 kontena master ssh -i $EC2_KEYS_DIR/kontena.pem \
-  docker run --name nrsysmond -d --restart always \
+  docker run \
+    -d \
+    --name nrsysmond \
+    --restart always \
+    --pid=host \
+    --net=host \
+    --privileged=true \
     -e NRSYSMOND_hostname=kontena/master \
     -e NRSYSMOND_license_key=$NEWRELIC_LICENSE \
     -e NRSYSMOND_cgroup_style=0 \
-    -e NRSYSMOND_host_root=/host \
-    -e NRSYSMOND_cgroup_root=/host/sys/fs/cgroup \
     -e NRSYSMOND_loglevel=debug \
-    -v /proc:/host/proc:ro \
-    -v /dev:/host/dev:ro \
-    -v /sys/fs/cgroup:/host/sys/fs/cgroup:ro \
+    -v /dev:/dev:ro \
+    -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
     -v /var/run/docker.sock:/var/run/docker.sock \
     christianbladescb/newrelic-coreos
+
+    # -e NRSYSMOND_host_root=/host \
+    # -e NRSYSMOND_cgroup_root=/host/sys/fs/cgroup \
 
 # kontena master ssh -i $EC2_KEYS_DIR/kontena.pem \
 #   docker logs -f nrsysmond
