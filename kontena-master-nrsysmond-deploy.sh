e4ba0a5f8a4b55f5896a9ebd44d66331ca1f2c1b
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2017-05-18 23:02:36 +0200

    create script to start new relic drsysmond on the kontena master node

---
+++ kontena-master-nrsysmond-deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# To run NewRelic system monitor (nrsysmond) on the master node
+# start the christianbladescb/newrelic-coreos image
+
+# kontena master ssh -i $EC2_KEYS_DIR/kontena.pem \
+#   docker stop nrsysmond
+
+kontena master ssh -i $EC2_KEYS_DIR/kontena.pem \
+  docker run --name nrsysmond -d --restart always \
+    -e NRSYSMOND_hostname=kontena/master \
+    -e NRSYSMOND_license_key=$NEWRELIC_LICENSE \
+    -e NRSYSMOND_cgroup_style=0 \
+    -e NRSYSMOND_host_root=/host \
+    -e NRSYSMOND_cgroup_root=/host/sys/fs/cgroup \
+    -e NRSYSMOND_loglevel=debug \
+    -v /proc:/host/proc:ro \
+    -v /dev:/host/dev:ro \
+    -v /sys/fs/cgroup:/host/sys/fs/cgroup:ro \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    christianbladescb/newrelic-coreos
+
+# kontena master ssh -i $EC2_KEYS_DIR/kontena.pem \
+#   docker logs -f nrsysmond
