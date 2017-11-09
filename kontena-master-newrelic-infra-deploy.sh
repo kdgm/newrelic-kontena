9ded8e0fb9c836330cf91f0ea7541881029b54a4
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2017-11-09 14:29:37 +0100

    convert to newrelic-infra settings

--- kontena-master-newrelic-infra-deploy.sh
+++ kontena-master-newrelic-infra-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Run this script to start or update the NewRelic system monitor (nrsysmond)
-# on the master node.
+# Run this script to start or update the NewRelic Infrastructure monitor
+# (newrelic-infra) on the master node.
 
 function master_ssh {
   kontena master ssh -i $KONTENA_SSH_KEY $@
@@ -9,30 +9,22 @@ function master_ssh {
 
 # master_ssh docker ps
 
-master_ssh docker stop nrsysmond
+master_ssh docker stop newrelic-infra || true
 
-master_ssh docker rm nrsysmond
+master_ssh docker rm newrelic-infra || true
 
-# Use dockerk options from
-# https://github.com/christian-blades-cb/newrelic-docker/blob/master/newrelic.service
+# Use docker options from
+# https://docs.newrelic.com/docs/infrastructure/new-relic-infrastructure/data-instrumentation/monitor-containers-underlying-hosts-coreos
 
 master_ssh docker run \
     -d \
-    --name nrsysmond \
+    --name newrelic-infra \
     --restart always \
-    --pid=host \
-    --net=host \
-    --privileged=true \
-    -e NRSYSMOND_hostname=kontena/master \
-    -e NRSYSMOND_license_key=$NEWRELIC_LICENSE \
-    -e NRSYSMOND_cgroup_style=0 \
-    -v /dev:/dev:ro \
-    -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    christianbladescb/newrelic-coreos
+    --network=host \
+    -e NRIA_DISPLAY_NAME="kontena/master" \
+    -e NRIA_LICENSE_KEY="$NEWRELIC_LICENSE" \
+    -v "/:/host:ro" \
+    -v "/var/run/docker.sock:/var/run/docker.sock" \
+    newrelic/infrastructure:latest
 
-    # -e NRSYSMOND_loglevel=debug \
-    # -e NRSYSMOND_host_root=/host \
-    # -e NRSYSMOND_cgroup_root=/host/sys/fs/cgroup \
-
-# master_ssh docker logs -f nrsysmond
+# master_ssh docker logs -f newrelic-infra
